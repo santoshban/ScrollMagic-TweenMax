baa4710b54611d8c0d1707d04ea9097b288dde1f
santoshban <mail.santoshban@gmail.com>
2018-06-27 12:56:01 +1000

    GSAP practice

---
+++ js/main.js
@@ -0,0 +1,12 @@
+TweenMax.to(".spider", 5, {scale: 0.6});
+TweenMax.to(".spider", 5, {scale: 1.6});
+TweenMax.to(".spider", 5, {opacity: 0.9});
+
+TweenMax.to(".box", 2, {
+    x: 1200,
+    rotation: 360,
+    scale: 0.5,
+    borderRadius: "25%",
+    ease:Back.easeOut,
+    repeat: -1
+});
